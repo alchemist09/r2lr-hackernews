1ac7c3d0a829652b1b9e9262e96354d6c6e6a4f0
Luke <mugapedia@gmail.com>
2018-05-15 20:00:21 +0300

    Create UI elements from a list using JSX

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3, 
+    points: 4,
+    objectID: 0
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clarke',
+    num_comments: 2,
+    points: 6,
+    objectID: 1
+  }
+]
+
 class App extends Component {
   render() {
-    const welcome = "Welcome to the Road to learn React";
-    const person = {
-      firstname: "Robin",
-      lastname: "Weiruch"
-    }
     return (
       <div className="App">
-        <h2>{welcome}</h2>
-        <h3>Instructor</h3>
-        <p>Firstname: {person.firstname}</p>
-        <p>Lastname: {person.lastname}</p>
+        {
+          list.map((item) => 
+            <div key={item.objectID}>
+                <span><a href={item.url}>{item.title}</a></span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </div>
+          )
+        }
       </div>
     );
   }
