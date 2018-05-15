f9701362b240cb4360d1c28139a518ede88aadbe
Luke <mugapedia@gmail.com>
2018-05-15 22:25:20 +0300

    Manipulate component state using buttons to pass new state

--- src/App.js
+++ src/App.js
@@ -17,23 +17,48 @@ const list = [
     num_comments: 2,
     points: 6,
     objectID: 1
+  },
+  {
+    title: 'Angular',
+    url: 'https://angular.io',
+    author: 'Misko Hevery',
+    num_comments: 3,
+    points: 4, 
+    objectID: 2
   }
 ]
 
 class App extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       list,
       projectName: "Hacker News"
-    }
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+  toggleProjectName() {
+    let nameOfProject = this.state.projectName;
+    const nameOfProjectIsUpperCase = (nameOfProject === nameOfProject.toLocaleUpperCase());
+    nameOfProject = nameOfProjectIsUpperCase ? nameOfProject.toLocaleLowerCase() : nameOfProject.toLocaleUpperCase();
+    this.setState({ projectName: nameOfProject });
   }
 
   render() {
     return (
       <div className="App">
-        <h2>{this.state.projectName}</h2>
+        <h2>{
+          this.state.projectName} 
+          <button type="button"
+                  onClick={() => this.toggleProjectName()}>Toggle Case</button>
+        </h2>
         {
           this.state.list.map(item => 
             <div key={item.objectID}>
@@ -41,6 +66,12 @@ class App extends Component {
                 <span>{item.author}</span>
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
+                <span>
+                  <button onClick={() => this.onDismiss(item.objectID)}
+                          type="button"> 
+                    Dismiss
+                  </button>
+                </span>
             </div>
           )
         }
