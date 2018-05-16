9255399cb1b218ad655f4cfa6e564a40d131f69a
Luke <mugapedia@gmail.com>
2018-05-16 21:41:10 +0300

    Create Search and Table components from the App component

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const list = [
     points: 4, 
     objectID: 2
   }
-]
+];
 
 const isSearched = searchTerm => item => !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -38,14 +38,11 @@ class App extends Component {
       list,
       projectName: "Hacker News",
       searchTerm: ''
-    };
+    };    
+
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
-  }
-
-  onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    this.toggleProjectName = this.toggleProjectName.bind(this);
   }
 
   toggleProjectName() {
@@ -55,12 +52,17 @@ class App extends Component {
     this.setState({ projectName: nameOfProject });
   }
 
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
 
   render() {
-    const { list, projectName, searchTerm } = this.state;
+    const {list, projectName, searchTerm } = this.state;
     return (
       <div className="App">
         <h2>
@@ -69,20 +71,51 @@ class App extends Component {
                   onClick={() => this.toggleProjectName()}>Toggle Case</button>
         </h2>
 
-        <form>
-          <input type="text"
-                 onChange={ this.onSearchChange } 
-          />
-        </form>
+        <Search
+         value={searchTerm}
+         onChange={this.onSearchChange}
+        />
+
+        <Table
+         list={list}
+         pattern={searchTerm}
+         onDismiss={this.onDismiss}
+        />
+        
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
         {
-          list.filter(isSearched(searchTerm)).map(item => 
+          list.filter(isSearched(pattern)).map(item => 
             <div key={item.objectID}>
                 <span><a href={item.url}>{item.title}</a></span>
                 <span>{item.author}</span>
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
                 <span>
-                  <button onClick={() => this.onDismiss(item.objectID)}
+                  <button onClick={() => onDismiss(item.objectID)}
                           type="button"> 
                     Dismiss
                   </button>
