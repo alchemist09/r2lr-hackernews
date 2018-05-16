3475749884e699225ba2faaa1655c17cdbcf2876
Luke <mugapedia@gmail.com>
2018-05-16 18:26:17 +0300

    Change project title, Refactor component state in render method to make use of ES6 destructuring

--- src/App.js
+++ src/App.js
@@ -60,10 +60,11 @@ class App extends Component {
   }
 
   render() {
+    const { list, projectName, searchTerm } = this.state;
     return (
       <div className="App">
-        <h2>{
-          this.state.projectName} 
+        <h2>
+          {projectName} 
           <button type="button"
                   onClick={() => this.toggleProjectName()}>Toggle Case</button>
         </h2>
@@ -74,7 +75,7 @@ class App extends Component {
           />
         </form>
         {
-          this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
+          list.filter(isSearched(searchTerm)).map(item => 
             <div key={item.objectID}>
                 <span><a href={item.url}>{item.title}</a></span>
                 <span>{item.author}</span>
