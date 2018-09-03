03596fe816a7a35c1b2efe376caacf74c0098828
Luke <mugapedia@gmail.com>
2018-09-03 08:34:44 +0300

    Removes unused imports from file

--- src/components/Table/test.js
+++ src/components/Table/test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import { App } from '../App';
 import { Table } from './index';
-import { Search } from '../Search';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+
+configure({ adapter: new Adapter() });
 
 describe('Table', () => {
 
@@ -26,5 +29,13 @@ describe('Table', () => {
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  })
 
 });
