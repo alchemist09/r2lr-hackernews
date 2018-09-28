eeb2b3c44096aca0c0599df4579afa0e6c67de43
Luke <mugapedia@gmail.com>
2018-09-28 10:19:28 +0300

    Removes the type prop from Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
 
-export const Button = ({onClick, className, type, children}) =>
+export const Button = ({onClick, className, children}) =>
   <button
    onClick={onClick}
    className={className}
+   type="button"
   >
     {children}
   </button>
 
 Button.defaultProps = {
   className: '',
-  type: 'button'
+  // type: 'button'
 }
 
 Button.propTypes = {
