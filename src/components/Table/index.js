3be5fc199f12d3e7f734ad48caabea010683c312
Luke <mugapedia@gmail.com>
2018-10-03 18:59:19 +0300

    Adds sort buttons as headers to the table component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,14 +3,58 @@ import PropTypes from 'prop-types';
 import { Button } from '../Button';
 import './Table.css';
 
+import { sortBy } from 'lodash';
+import { Sort } from '../Sort';
+
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'author'),
+  COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+  POINTS: list => sortBy(list, 'points').reverse()
+}
+
 const largeColumn = { width: '40%' }
 const midColumn = { width: '30%' }
 const smallColumn = { width: '10%' }
 
-export const Table = ({list, onDismiss}) =>
+export const Table = ({list, onDismiss, sortKey, onSort }) =>
   <div className="table">
+    <div className="table-header">
+      <span style={{ width: '40%' }}>
+        <Sort
+          sortKey={'TITLE'}
+          onSort={onSort}>
+          Title  
+        </Sort>
+      </span>
+      <span style={{ width: '30%'}}>
+        <Sort
+          sortKey={'Author'}
+          onSort={onSort}>
+          Author  
+        </Sort>
+      </span>
+      <span style={{ width: '10%' }}>
+        <Sort
+          sortKey={'COMMENTS'}
+          onSort={onSort}>
+          Comments  
+        </Sort>
+      </span>
+      <span style={{ width: '10%' }}>
+        <Sort
+          sortKey={'POINTS'}
+          onSort={onSort}>
+          Points
+        </Sort>
+      </span>
+      <span style={{ width: '10%' }}>
+        Archive
+      </span>
+    </div>
     {
-      list.map(item => 
+      SORTS[sortKey](list).map(item => 
         <div key={item.objectID} className="table-row">
           <span style={largeColumn}>
             <a href={item.url}>{item.title}</a>
