28da1ed00ce879e717732bab3f0e7806efcd8b8e
Luke <mugapedia@gmail.com>
2018-10-09 11:16:23 +0300

    Changes Table component to stateful ES6 class component. Moves state specific to this component from parent App component to within the component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '../Button';
 import './Table.css';
@@ -18,75 +18,94 @@ const largeColumn = { width: '40%' }
 const midColumn = { width: '30%' }
 const smallColumn = { width: '10%' }
 
-export const Table = ({list, onDismiss, sortKey, onSort, isSortReverse }) => {
-  const sortedList = SORTS[sortKey](list);
-  const reverseSortedList = isSortReverse ? sortedList.reverse() : sortedList;
-  return (
-    <div className="table">
-    <div className="table-header">
-      <span style={{ width: '40%' }}>
-        <Sort
-          sortKey={'TITLE'}
-          onSort={onSort}
-          activeSortKey={sortKey}>
-          Title  
-        </Sort>
-      </span>
-      <span style={{ width: '30%'}}>
-        <Sort
-          sortKey={'AUTHOR'}
-          onSort={onSort}
-          activeSortKey={sortKey}>
-          Author  
-        </Sort>
-      </span>
-      <span style={{ width: '10%' }}>
-        <Sort
-          sortKey={'COMMENTS'}
-          onSort={onSort}
-          activeSortKey={sortKey}>
-          Comments  
-        </Sort>
-      </span>
-      <span style={{ width: '10%' }}>
-        <Sort
-          sortKey={'POINTS'}
-          onSort={onSort}
-          activeSortKey={sortKey}>
-          Points
-        </Sort>
-      </span>
-      <span style={{ width: '10%' }}>
-        Archive
-      </span>
-    </div>
-    {
-      reverseSortedList.map(item => 
-        <div key={item.objectID} className="table-row">
-          <span style={largeColumn}>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span style={midColumn}>{
-            item.author}
-          </span>
-          <span style={smallColumn}>
-            {item.num_comments}
-          </span>
-          <span style={smallColumn}>
-            {item.points}
-          </span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)}
-                    className="button-inline"
-            >
-              Dismiss
-            </Button>
-          </span>
-        </div>
-      )
+export class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortKey: 'NONE',
+      isSortReverse: false
     }
-    </div>
-  )
+    this.onSort = this.onSort.bind(this);
+  }
+
+  onSort(sortKey) {
+    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse;
+    this.setState({ sortKey, isSortReverse });
+  }
+
+  render () {
+    const { list, onDismiss } = this.props
+    const { sortKey, isSortReverse } = this.state;
+    const sortedList = SORTS[sortKey](list);
+    const reverseSortedList = isSortReverse ? sortedList.reverse() : sortedList;
+
+    return (
+      <div className="table">
+      <div className="table-header">
+        <span style={{ width: '40%' }}>
+          <Sort
+            sortKey={'TITLE'}
+            onSort={this.onSort}
+            activeSortKey={sortKey}>
+            Title  
+          </Sort>
+        </span>
+        <span style={{ width: '30%'}}>
+          <Sort
+            sortKey={'AUTHOR'}
+            onSort={this.onSort}
+            activeSortKey={sortKey}>
+            Author  
+          </Sort>
+        </span>
+        <span style={{ width: '10%' }}>
+          <Sort
+            sortKey={'COMMENTS'}
+            onSort={this.onSort}
+            activeSortKey={sortKey}>
+            Comments  
+          </Sort>
+        </span>
+        <span style={{ width: '10%' }}>
+          <Sort
+            sortKey={'POINTS'}
+            onSort={this.onSort}
+            activeSortKey={sortKey}>
+            Points
+          </Sort>
+        </span>
+        <span style={{ width: '10%' }}>
+          Archive
+        </span>
+      </div>
+      {
+        reverseSortedList.map(item => 
+          <div key={item.objectID} className="table-row">
+            <span style={largeColumn}>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={midColumn}>{
+              item.author}
+            </span>
+            <span style={smallColumn}>
+              {item.num_comments}
+            </span>
+            <span style={smallColumn}>
+              {item.points}
+            </span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}
+                      className="button-inline"
+              >
+                Dismiss
+              </Button>
+            </span>
+          </div>
+        )
+      }
+      </div>
+    )
+  }
 }
 
   
